b18e4e455380c4b3d7fd4312adaf9fa0eb9a75eb
jairo.rios <jairo199316@hotmail.com>
2020-01-10 11:47:27 -0500

    Se agregan pruebas unitarias

--- AlquilerVehiculosFront/app/src/main/java/com/example/alquilervehiculosfront/datos/restutil/UrlServicio.java
+++ AlquilerVehiculosFront/app/src/main/java/com/example/alquilervehiculosfront/datos/restutil/UrlServicio.java
@@ -2,6 +2,7 @@ package com.example.alquilervehiculosfront.datos.restutil;
 
 public class UrlServicio {
 
-    public static final String URL_BASE = "http://192.168.43.252:8080/";
+    //public static final String URL_BASE = "http://192.168.43.252:8080/";
+    public static final String URL_BASE = "http://192.168.13.22:8080/";
 
 }
